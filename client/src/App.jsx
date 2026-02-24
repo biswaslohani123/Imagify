8be9b46d73b7ebcd3accbfcf6fd230d18262efe9
biswasuser <lohanibiswas1@gmail.com>
2026-02-24 15:27:03 +0545

    made home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/result' element={<Result/>}/>
+        <Route path='/buy' element={<BuyCredit/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return (
+    <div>
+       <Link to='/'>
+          <img src={assets.logo} alt="" className='w-28 sm:w-32 lg:w-40' />
+       </Link>
+
+       <div>
+            <div></div>
+            <div></div>
+       </div>
+    </div>
+  )
+}
+
+export default NavBar
