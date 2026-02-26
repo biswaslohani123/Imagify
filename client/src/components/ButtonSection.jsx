83508d3cc4caac48c821ed186b7456db8462bf84
biswasuser <lohanibiswas1@gmail.com>
2026-02-26 13:48:23 +0545

    completd button section

---
+++ client/src/components/ButtonSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const ButtonSection = () => {
+  return (
+    <div className='mt-32 items-center flex justify-center flex-col'>
+    
+        <h1 className='text-3xl sm:text-4xl font-semibold'>See the magic. Try now</h1>
+
+         <button className="sm:text-lg text-white bg-black px-7 py-3 flex justify-center items-center gap-2 rounded-4xl mt-6 cursor-pointer">
+                Generate Images <img className="w-5 h-5" src={assets.star_group} alt="" />
+            </button>
+      
+    </div>
+  )
+}
+
+export default ButtonSection
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Description from '../components/Description'
 import Testomonialsection from '../components/Testomonialsection'
+import ButtonSection from '../components/ButtonSection'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Steps/>
       <Description/>
       <Testomonialsection/>
+      <ButtonSection/>
     </div>
   )
 }
